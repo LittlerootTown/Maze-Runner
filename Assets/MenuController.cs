49e5c1df4c37325c17692937eea479515c53672a
Bar Merchav <121390299+BarTriesHisBest@users.noreply.github.com>
2024-09-04 17:37:25 +0300

    Added Menu

---
+++ Assets/MenuController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Method to start the game
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    // Method to quit the game
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stop play mode in the editor
+#else
+        Application.Quit(); // Quit the application in a build
+#endif
+    }
+}
